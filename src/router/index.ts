73afa64426331aaee080ab2882fc695a0f9d9174
RhysDevalckeneer2 <rhys.devalckeneer@hotmail.be>
2023-03-09 14:19:48 +0100

    refactor(routes): update routes
    Added sessions routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -10,6 +10,33 @@ const routes: Array<RouteRecordRaw> = [
         component: HomeView
    },
    {
+        path: '/signup',
+        name: 'Signup',
+        component: () => import('../views/Auth/SignupView.vue')
+    },
+    {
+        path: '/login',
+        name: 'Login',
+        component: () => import('../views/Auth/LoginView.vue')
+    },
+    {
+        path: '/sessions',
+        children: [
+            {
+                path: "create",
+                component: () => import('../views/Table/TableView.vue')
+            },
+            {
+                path: "join",
+                component: () => import('../views/Table/TableView.vue')
+            },
+            {
+                path: ":id",
+                component: () => import('../views/Table/TableView.vue')
+            },
+        ]
+    },
+   {
         path: '/:catchAll(.*)',
         name: 'NotFound',
         component: () => import('../views/Error/NotFound.vue')
